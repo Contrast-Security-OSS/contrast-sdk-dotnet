2c1b1f04bac691a4bcbf6d040f07ea39809eaa75
rduran0 <rduran@iwa.com.mx>
2018-11-02 08:52:49 -0600

    CONTRAST-28109 (#12)
    * CONTRAST-28109
    
    * Added endpoint and fields to retrieve an organization data.
    * Added mock test for the endpoint.
    
    * CONTRAST-28109
    
    * Replaced cycle with function call.

--- contrast-rest-dotnet/Model/Organization.cs
+++ contrast-rest-dotnet/Model/Organization.cs
@@ -26,21 +26,134 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+using contrast_rest_dotnet.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace contrast_rest_dotnet.Model
 {
+    public enum ServerEnvironment
+    {
+        DEVELOPMENT,
+        QA,
+        PRODUCTION
+    }
+
     public class Organization
     {
+        /// <summary>
+        /// Organization name
+        /// </summary>
         public string name { get; set; }
+
         public string shortname { get; set; }
+
+        /// <summary>
+        /// Organization time zone
+        /// </summary>
         public string timezone { get; set; }
+
         public List<Link> links { get; set; }
+
+        /// <summary>
+        /// Organization ID
+        /// </summary>
         public string organization_uuid { get; set; }
+
+        /// <summary>
+        /// Account ID
+        /// </summary>
+        [JsonProperty(PropertyName = "account_id")]
+        public String AccountId { get; set; }
+
+        /// <summary>
+        /// Number of applications onboarded
+        /// </summary>
+        [JsonProperty(PropertyName = "apps_onboarded")]
+        public long? AppsOnboarded { get; set; }
+
+        /// <summary>
+        /// Auto license assessment
+        /// </summary>
+        [JsonProperty(PropertyName = "auto_license_assessment")]
+        public bool AutoLicenseAssessment { get; set; }
+
+        /// <summary>
+        /// Auto license protetion
+        /// </summary>
+        [JsonProperty(PropertyName = "auto_license_protection")]
+        public bool AutoLicenseProtection { get; set; }
+
+        /// <summary>
+        /// Organization creation time
+        /// </summary>
+        [JsonConverter(typeof(EpochDateTimeConverter))]
+        [JsonProperty(PropertyName = "creation_time")]
+        public DateTime? CreationTime { get; set; }
+
+        /// <summary>
+        /// Is this organization freemium?
+        /// </summary>
+        [JsonProperty(PropertyName = "freemium")]
+        public bool? IsFreemium { get; set; }
+
+        /// <summary>
+        /// Is user guest in this organization
+        /// </summary>
+        [JsonProperty(PropertyName = "guest")]
+        public bool? IsGuest { get; set; }
+
+        /// <summary>
+        /// Is a Superadmin Organization
+        /// </summary>
+        [JsonProperty(PropertyName = "is_superadmin")]
+        public bool? IsSuperadmin { get; set; }
+
+        /// <summary>
+        /// Has user protect enabled in this organization?
+        /// </summary>
+        [JsonProperty(PropertyName = "protect")]
+        public bool? IsProtect { get; set; }
+
+        /// <summary>
+        /// Protection enabled
+        /// </summary>
+        [JsonProperty(PropertyName = "protection_enabled")]
+        public bool IsProtectionEnabled { get; set; }
+
+        /// <summary>
+        /// Sample application ID
+        /// </summary>
+        [JsonProperty(PropertyName = "sample_application_id")]
+        public String SampleAppId { get; set; }
+
+        /// <summary>
+        /// Sample server ID
+        /// </summary>
+        [JsonProperty(PropertyName = "sample_server_id")]
+        public long? SampleServerId { get; set; }
+
+        /// <summary>
+        /// List of server environments
+        /// </summary>
+        [JsonProperty(PropertyName = "server_environments")]
+        public List<ServerEnvironment> ServerEnvironments { get; set; }
+
+        [JsonProperty(PropertyName = "superadmin")]
+        public bool? Superadmin { get; set; }
+
+        /// <summary>
+        /// Organization date format
+        /// </summary>
+        [JsonProperty(PropertyName = "date_format")]
+        public string DateFormat { get; set; }
+
+        /// <summary>
+        /// Organization time format
+        /// </summary>
+        [JsonProperty(PropertyName = "time_format")]
+        public string TimeFormat { get; set; }
     }
 
     public class OrganizationResponse
@@ -57,6 +170,30 @@ namespace contrast_rest_dotnet.Model
         public Organization organization { get; set; }
         public List<string> roles { get; set; }
         public bool enterprise { get; set; }
+    }
+
+    /// <summary>
+    /// Organization Managed Response
+    /// </summary>
+    public class OrganizationManagedResponse
+    {
+        /// <summary>
+        /// If user accounts are managed by Contrast
+        /// </summary>
+        [JsonProperty(PropertyName = "managed")]
+        public bool Managed { get; set; }
+
+        [JsonProperty(PropertyName = "messages")]
+        public List<string> Messages { get; set; }
+
+        /// <summary>
+        /// Organization resource
+        /// </summary>
+        [JsonProperty(PropertyName = "organization")]
+        public Organization Organization { get; set; }
+
+        [JsonProperty(PropertyName = "success")]
+        public bool Success { get; set; }
     }
 
 }
